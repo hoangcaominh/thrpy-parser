8e073a812e93e1fef8029f47d0813af6786778fe
hoangcaominh <hoangcaominh1812@gmail.com>
2023-11-12 20:34:33 +0700

    Create type definitions

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+export const PARSERS: object
+
+export function parse(replay: Buffer): object
+export function parse06(replay: Buffer): object
+export function parse07(replay: Buffer): object
+export function parse08(replay: Buffer): object
+export function parse09(replay: Buffer): object
+export function parse10(replay: Buffer): object
+export function parse11(replay: Buffer): object
+export function parse12(replay: Buffer): object
+export function parse128(replay: Buffer): object
+export function parse13(replay: Buffer): object
+export function parse14(replay: Buffer): object
+export function parse13or14(replay: Buffer): object
+export function parse15(replay: Buffer): object
+export function parse16(replay: Buffer): object
+export function parse17(replay: Buffer): object
+export function parse18(replay: Buffer): object
+
+export const CONSTANTS: object
